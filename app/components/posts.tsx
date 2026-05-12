709cd43ce35bbd3486c5fc4b8ae96b59851d3875
Marvin Tunji-ola <marvinsmart9@gmail.com>
2026-05-12 01:51:25 +0100

    Update layout in ThoughtsPosts component: adjust width properties for improved responsiveness and maintain visual consistency across different screen sizes.

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -25,10 +25,10 @@ export function ThoughtsPosts({ showAll = false }: ThoughtsPostsProps) {
           .map((post) => (
             <HoverPreviewLink
               key={post.slug}
-              className="flex flex-col space-y-1 mb-2"
+              className="flex flex-col space-y-1 mb-2 w-fit max-w-full"
               href={`/thoughts/${post.slug}`}
             >
-              <div className="w-full flex flex-col md:flex-row md:items-center space-x-0 md:space-x-2">
+              <div className="flex flex-col md:flex-row md:items-center space-x-0 md:space-x-2">
                 <div className="flex items-center space-x-2 text-sm">
                   <HashIcon className="w-4 h-4 text-neutral-900" />
                   <p className="text-neutral-400 w-[80px] whitespace-nowrap shrink-0">
